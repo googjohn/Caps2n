22e79639f3ce90c642088e305a345cdba7c5c716
googjohn <120353235+googjohn@users.noreply.github.com>
2023-06-26 12:26:57 +0800

    try deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import Logo from '../assets/logo-no-background.png';
 import ReactLogo from '../assets/logo-react.png';
 import './Header.css';
 const Header = () => {
-  const activeClass =
-    "block text-base py-2 pl-3 pr-4 text-white bg-green md:bg-transparent md:text-green md:p-0 md:dark:text-green";
+  // const activeClass =
+  //   "block text-base py-2 pl-3 pr-4 text-white bg-green md:bg-transparent md:text-green md:p-0 md:dark:text-green";
 
-  const inActiveClass = "block text-base py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+  // const inActiveClass = "block text-base py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
 
   return (
 
